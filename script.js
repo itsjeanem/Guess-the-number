0db215567438e27a85d186a7f07faa12c210c434
ItsJeanem <Jeanem@duck.com>
2023-05-27 11:56:01 +0000

    Guess the number with the enter button

--- script.js
+++ script.js
@@ -61,6 +61,50 @@ guessBtn.addEventListener("click", () => {
     }
 });
 
+window.addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+        event.preventDefault();
+
+        let inputNumber = document.getElementById("number").value;
+        let returnText = document.getElementById("return_text");
+        let trailText = document.getElementById("trials_text");
+        let numberOfTrials = 5;
+
+        const { action } = startBtn.dataset;
+        if (action === "start") {
+            alert('Press the "start" button to start the game.');
+        } else {
+            if (inputNumber === "") {
+                alert("Enter a number to play");
+                trailText.innerText = `Number of tests remaining: ${numberOfTrials}`;
+            } else {
+                if (inputNumber < 1) {
+                    alert("Enter a number between 1 and 100");
+                } else if (inputNumber > 100) {
+                    alert("Enter a number between 1 and 100");
+                } else {
+                    if (numberOfTrials > 0) {
+                        if (mysteryNumber > inputNumber) {
+                            returnText.textContent = `${inputNumber} is less than the mystery number`;
+                            numberOfTrials -= 1;
+                            trailText.innerText = `Number of tests remaining: ${numberOfTrials}`;
+                        } else if (mysteryNumber < inputNumber) {
+                            returnText.textContent = `${inputNumber} is greater than the mystery number`;
+                            numberOfTrials -= 1;
+                            trailText.innerText = `Number of tests remaining: ${numberOfTrials}`;
+                        } else {
+                            returnText.textContent = `Congratulations, the mystery number was ${inputNumber}`;
+                            trailText.innerText = "";
+                        }
+                    } else {
+                        returnText.textContent = `Sorry you lose, the mystery number was ${inputNumber}`;
+                    }
+                }
+            }
+        }
+    }
+});
+
 function stopGame() {
     window.location.reload();
 }
